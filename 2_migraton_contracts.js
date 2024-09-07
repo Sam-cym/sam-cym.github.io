c3d15e6543931c24ee5dffea67b8d1bff283c710
Sam-cym <promisesoore@gmail.com>
2024-09-07 16:24:21 +0100

    2_migraton_contracts.js

--- 2_migraton_contracts.js
+++ 2_migraton_contracts.js
@@ -1,5 +1,7 @@
 const HealthDataManagement = artifacts.require("HealthDataManagement");
 
-module.exports = function (deployer) {
-  deployer.deploy(HealthDataManagement);
+module.exports = async function (deployer) {
+    await deployer.deploy(HealthDataManagement);
+    const instance = await HealthDataManagement.deployed();
+    console.log("HealthDataManagement deployed at address:", instance.address);
 };
